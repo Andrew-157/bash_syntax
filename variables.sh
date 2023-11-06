63526d5a792a82964e46b85419b42bb8bd979ae8
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-06 13:09:15 +0100

    docs: use standard input in the script

--- variables.sh
+++ variables.sh
@@ -53,3 +53,52 @@ export var1
 
 # Exporting variables is a one way process. The original process may pass variables over to the new process but anything that process 
 # does with the copy of the variables has no impact on the original variables.
+
+
+# Bash accomodates piping and redirection by way of special files. Each process gets 
+# it's own set of files (one for STDIN, STDOUT and STDERR respectively) and they are linked when 
+# piping or redirection is invoked. Each process gets the following files:
+
+    # STDIN - /proc/<processID>/fd/0
+    # STDOUT - /proc/<processID>/fd/1
+    # STDERR - /proc/<processID>/fd/2
+
+
+# To make life more convenient the system creates some shortcuts for us:
+
+#     STDIN - /dev/stdin or /proc/self/fd/0
+#     STDOUT - /dev/stdout or /proc/self/fd/1
+#     STDERR - /dev/stderr or /proc/self/fd/2
+
+# fd in the paths above stands for file descriptor.
+
+# So if we would like to make our script able to process data that is piped to it all we 
+# need to do is read the relevant file. 
+# All of the files mentioned above behave like normal files.
+
+# File report.sh
+
+#!/bin/bash
+# A basic summary of my sales report
+echo Here is a summary of the sales data:
+echo ====================================
+echo
+cat /dev/stdin | cut -d' ' -f 2,3 | sort
+
+# So, if we have file salesdata.txt with the following content:
+Fred apples 20 November 4
+Susy oranges 5 November 7
+Mark watermelons 12 November 10
+Terry peaches 7 November 15
+
+# We can call:
+cat salesdata.txt | ./report.sh
+
+# The output:
+
+Here is a summary of the sales data:
+====================================
+apples 20
+oranges 5
+peaches 7
+watermelons 12
