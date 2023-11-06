b377f5aba8f18342095a807f76f1be0b40072861
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-06 16:00:05 +0100

    docs: case statement

--- if_statements.sh
+++ if_statements.sh
@@ -65,19 +65,3 @@ else
     ls
 fi
 
-
-# case example
-case $1 in
-    start)
-        echo starting
-        ;;
-    stop)
-        echo stoping
-        ;;
-    restart)
-        echo restarting
-        ;;
-    *)
-        echo don\'t know
-        ;;
-esac
